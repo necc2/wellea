78c1f0cc42ec1ba890bcf297ab45af380e40c383
necc2 <y9t55ks9f6@privaterelay.appleid.com>
2026-09-17 14:33:05 +0900

    Add initial news data for wellea activities

---
+++ news.js
@@ -0,0 +1,27 @@
+const newsData = [
+  {
+    date: "2026.09.16",
+    title: "wellea 活動開始のお知らせ(仮)",
+    new: true
+  },
+  {
+    date: "2026.09.15",
+    title: "歌ってみたグループ「wellea」始動！(仮)",
+    new: false
+  },
+  {
+    date: "2026.09.14",
+    title: "wellea公式サイトを公開しました(仮)",
+    new: false
+  },
+  {
+    date: "2026.09.13",
+    title: "wellea公式SNSを開設しました(仮)",
+    new: false
+  },
+  {
+    date: "2026.09.12",
+    title: "メンバー紹介ページを公開しました(仮)",
+    new: false
+  }
+];
